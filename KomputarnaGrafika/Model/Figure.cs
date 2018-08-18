97f952a3d1b9f78a1e4f94e5caae1b0131cc8836
Bat Vasko <tigerateme@yandex.ru>
2018-08-18 14:12:27 +0300

    Add project files.

---
+++ KomputarnaGrafika/Model/Figure.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace KomputarnaGrafika
+{
+    class Figure
+    {
+        public bool IsSelected { get; set; }
+        public RectangleF RectF { get; set; }
+        public Color Color { get; set; }
+        public Pen Pen { get; set; }
+        public Matrix Matrix { get; set; }
+        public string Name { get; set; }
+
+
+        public virtual bool IsInside(PointF point)
+        {
+            return true;
+        }
+
+        public virtual void Draw(Graphics grfx)
+        {
+
+        }
+
+        public virtual void ChangePenColor(Color color)
+        {
+            this.Pen.Color = color;
+        }
+
+        public virtual void ChangePenWidth(float width)
+        {
+            this.Pen.Width = width;
+        }
+
+        public virtual void Translate(float x, float y)
+        {
+            Matrix translationMatrix = new Matrix();
+            translationMatrix.Translate(x, y);
+            this.Matrix.Multiply(translationMatrix, MatrixOrder.Append);
+        }
+
+        public virtual void Rotate(float degrees, PointF point)
+        {
+            Matrix rotationMatrix = new Matrix();
+            rotationMatrix.RotateAt(degrees, point);
+            this.Matrix.Multiply(rotationMatrix, MatrixOrder.Append);
+        }
+
+        public virtual void RotateAt(float degrees)
+        {
+            PointF rotationPoint = this.GetMiddlePoint();
+
+            this.Rotate(degrees, rotationPoint);
+        }
+
+        protected PointF GetMiddlePoint()
+        {
+            RectangleF bounds = GetBounds();
+
+            PointF mid = new PointF(bounds.Width / 2 + bounds.X, bounds.Height / 2 + bounds.Y);
+
+            return mid;
+        }
+
+        protected RectangleF GetBounds()
+        {
+            PointF p1, p2, p3, p4;
+            p1 = new PointF(this.RectF.X, this.RectF.Y);
+            p2 = new PointF(this.RectF.X, this.RectF.Y + this.RectF.Height);
+            p3 = new PointF(this.RectF.X + this.RectF.Width, this.RectF.Y);
+            p4 = new PointF(this.RectF.X + this.RectF.Width, this.RectF.Y + this.RectF.Height);
+            PointF[] points = new PointF[4] { p1, p2, p3, p4 };
+            this.Matrix.TransformPoints(points);
+
+            float minx, maxx, miny, maxy;
+            minx = points[0].X;
+            maxx = points[0].X;
+            miny = points[0].Y;
+            maxy = points[0].Y;
+            for (int i = 0; i <= 3; i++)
+            {
+                minx = Math.Min(minx, points[i].X);
+                miny = Math.Min(miny, points[i].Y);
+                maxx = Math.Max(maxx, points[i].X);
+                maxy = Math.Max(maxy, points[i].Y);
+            }
+
+            RectangleF bounds = new RectangleF(minx, miny, (maxx - minx), (maxy - miny));
+
+            return bounds;
+        }
+    }
+}
---
+++ KomputarnaGrafika/Model/Group.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+
+namespace KomputarnaGrafika.Model
+{
+    class Group : Figure
+    {
+        public List<Figure> GroupedFigures { get; set; }
+
+        public Group(List<Figure> figures)
+        {
+            this.GroupedFigures = new List<Figure>();
+            foreach (var figure in figures)
+            {
+                this.GroupedFigures.Add(figure);
+            }
+
+            this.Pen = new Pen(Color.Black, 1f);
+            this.Matrix = new Matrix();
+            this.Name = "Group";
+            CreateGroupRect();
+        }
+
+        public override bool IsInside(PointF point)
+        {
+            foreach (var figure in this.GroupedFigures)
+            {
+                if (figure.IsInside(point))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        public override void Translate(float x, float y)
+        {
+            base.Translate(x, y);
+
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.Translate(x, y);
+            }
+        }
+
+        public override void Rotate(float degrees, PointF point)
+        {
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.Rotate(degrees, point);
+            }
+        }
+
+        public override void RotateAt(float degrees)
+        {
+            PointF rotationPoint = this.GetMiddlePoint();
+
+            base.Rotate(degrees, rotationPoint);
+
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.Rotate(degrees, rotationPoint);
+            }
+        }
+
+        public override void Draw(Graphics grfx)
+        {
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.Draw(grfx);
+            }
+        }
+
+        public override void ChangePenColor(Color color)
+        {
+            this.Pen.Color = color;
+
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.ChangePenColor(color);
+            }
+        }
+
+        public override void ChangePenWidth(float width)
+        {
+            this.Pen.Width = width;
+
+            foreach (var figure in this.GroupedFigures)
+            {
+                figure.ChangePenWidth(width);
+            }
+        }
+
+        private void CreateGroupRect()
+        {
+            float minX = this.GroupedFigures[0].RectF.X;
+            float minY = this.GroupedFigures[0].RectF.Y;
+            float maxX = this.GroupedFigures[0].RectF.X + this.GroupedFigures[0].RectF.Width;
+            float maxY = this.GroupedFigures[0].RectF.Y + this.GroupedFigures[0].RectF.Height;
+
+            foreach (var figure in this.GroupedFigures)
+            {
+                if (minX > figure.RectF.X)
+                {
+                    minX = figure.RectF.X;
+                }
+
+                if (minY > figure.RectF.Y)
+                {
+                    minY = figure.RectF.Y;
+                }
+
+                if (maxX < (figure.RectF.X + figure.RectF.Width))
+                {
+                    maxX = figure.RectF.X + figure.RectF.Width;
+                }
+
+                if (maxY < (figure.RectF.Y + figure.RectF.Height))
+                {
+                    maxY = figure.RectF.Y + figure.RectF.Height;
+                }
+            }
+
+            this.RectF = new RectangleF(minX, minY, Math.Abs(maxX - minX), Math.Abs(maxY - minY));
+        }
+    }
+}
